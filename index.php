ccd3a5f7d8330fd0bb4acdb5c0847d0b49aa9e32
Junny Noriega <junnynoriega@gmail.com>
2023-01-31 02:34:09 -0500

    updated files

--- index.php
+++ index.php
@@ -26,5 +26,22 @@ $f3->route('GET /breakfast', function () {
     echo $view->render('views/breakfast.html');
 });
 
+// Define a order1 route (328/diner/breakfast)
+$f3->route('GET|POST /order1', function ($f3) {
+    //If the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Move data from POST array to SESSION array
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+    }
+
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/order-form1.html');
+});
+
 // Run Fat Free
 $f3->run();
