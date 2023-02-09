f62551f2bc708cd7421cd47bf1838ab0c5a9f18d
Junny Noriega <junnynoriega@gmail.com>
2023-02-09 16:48:32 -0500

    updated files

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+// Start Session
+session_start();
+
 // Require autoload file
 require_once('vendor/autoload.php');
+require_once('model/data-layer.php');
+//var_dump(getMeal());
 
 // Instantiate F3 Base class
 $f3 = Base::instance();
@@ -19,15 +24,16 @@ $f3->route('GET /', function () {
     echo $view->render('views/diner-home.html');
 });
 
-// Define a breakfast route (328/diner/breakfast)
-$f3->route('GET /breakfast', function () {
-    // Instantiate a view
-    $view = new Template();
-    echo $view->render('views/breakfast.html');
-});
+//// Define a breakfast route (328/diner/breakfast)
+//$f3->route('GET /breakfast', function () {
+//    // Instantiate a view
+//    $view = new Template();
+//    echo $view->render('views/breakfast.html');
+//});
 
-// Define a order1 route (328/diner/breakfast)
+// Define a order1 route (328/diner/order-form1)
 $f3->route('GET|POST /order1', function ($f3) {
+
     //If the form has been submitted
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         //Move data from POST array to SESSION array
@@ -35,12 +41,40 @@ $f3->route('GET|POST /order1', function ($f3) {
         $_SESSION['meal'] = $_POST['meal'];
 
         //Redirect to summary page
-        $f3->reroute('summary');
+        $f3->reroute('order2');
     }
+    // Add meals to F3 hive
+    $f3->set('meals',getMeal());
 
     // Instantiate a view
     $view = new Template();
     echo $view->render('views/order-form1.html');
+});
+
+// Define a default route (328/order2)
+$f3->route('GET|POST /order2', function ($f3) {
+    //If the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Move data from POST array to SESSION array
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+    }
+    // Add meals to F3 hive
+    $f3->set('condiments',getCond());
+
+    // Instantiate a view
+    $view = new Template();
+    echo $view->render('views/order-form2.html');
+});
+
+//Define a summary route (328/summary)
+$f3->route('GET /summary', function() {
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-summary.html");
+
 });
 
 // Run Fat Free
---
+++ model/data-layer.php
@@ -0,0 +1,9 @@
+<?php
+    function getMeal()
+    {
+        return array(" breakfast", " lunch", " dinner", " dessert");
+    }
+    function getCond()
+    {
+        return array(" Ketchup", " Mustard", " Sriracha", " BBQ");
+    }
